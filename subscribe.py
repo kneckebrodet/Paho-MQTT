aef0ebf04df4218e1b5d2001a2ccf76a33bb823a
kneckebrodet <113566464+kneckebrodet@users.noreply.github.com>
2023-04-23 12:50:57 +0900

    Update subscribe.py

--- subscribe.py
+++ subscribe.py
@@ -2,18 +2,21 @@ from paho.mqtt import client as mqtt
 
 def on_message(client, userdata, message):
     print("Received message:", str(message.payload.decode("shift-jis")))
-    
+
+broker = "localhost"    
+port = 1883
+
 client = mqtt.Client()
-client.username_pw_set(username="lucas", password="rohlin")
+client.username_pw_set(username="", password="")
 
 # Set callback function
 client.on_message = on_message
 
 # Connect to broker
-client.connect("192.168.3.11", 1883)
+client.connect(broker, port)
 
 # Subscribe to topic
-client.subscribe("test")
+client.subscribe("topic")
 
 # Loop and handle incoming messages
 client.loop_forever()
